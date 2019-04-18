690676308b66a59047b2d65d90a62dd2fb1edc17
Rafi Bagaskara Halilintar <masitingss@gmail.com>
2019-04-18 23:18:59 +0700

    Oke

--- app/init_menu.php
+++ app/init_menu.php
@@ -3,3 +3,8 @@
     {
         add_menu_page('Ninja Van', 'Ninja Van', 'manage_options', 'ninjavan_opt', 'main_view_pwavald', 'dashicons-admin-site');
     }
+?>
+
+<div style="float:right">
+	
+</div>
